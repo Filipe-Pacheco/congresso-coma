995c1ced051f08b8d684b0894f0e2573cde2d4d7
Filipe Pacheco <52587073+Filipe-Pacheco@users.noreply.github.com>
2021-04-19 12:44:53 -0300

    Add files via upload

---
+++ script-salas.js
@@ -0,0 +1,20 @@
+// Menu buttons
+var home_btn_element = document.getElementById("home-btn");
+var org_btn_element  = document.getElementById("org-btn");
+var programacao_btn_element  = document.getElementById("programacao-btn");
+var pj_cult_btn_element  = document.getElementById("pj-cult-btn");
+var patrocinadores_btn_element  = document.getElementById("patrocinadores-btn");
+var parcerias_btn_element  = document.getElementById("parcerias-btn");
+var instituicoes_btn_element  = document.getElementById("instituicoes-btn");
+var area_congressita_btn_element = document.getElementById("area_congressita_btn");
+
+home_btn_element.href = "index.html";
+org_btn_element.href = "organizadores.html";
+programacao_btn_element.href = "/programacao.html";
+pj_cult_btn_element.href = "/p-cultural.html";
+patrocinadores_btn_element.href = "/patrocinadores.html";
+parcerias_btn_element.href = "/parcerias.html";
+instituicoes_btn_element.href = "/instituicoes.html";
+area_congressita_btn_element.href = "/login.html";
+
+console.log("Sucesso ao abrir script das salas")
